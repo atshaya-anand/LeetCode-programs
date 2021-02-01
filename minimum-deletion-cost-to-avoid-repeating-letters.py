c3c43cda7c96ec8a483c91d856ce45f9e28066d4
Atshaya <atshaya1699@gmail.com>
2021-02-01 22:08:32 +0530

    added minimum deletion cost in py

---
+++ minimum-deletion-cost-to-avoid-repeating-letters.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCost(self, s: str, cost: List[int]) -> int:
+            res = 0
+            minn = 0
+            cost_copy = []
+            for i in range(len(s)):
+                if i != len(s)-1:
+                    j = i+1
+                    if j<=len(s):
+                        if j == len(s):
+                            if s[i] == s[j-1]:
+                                cost_copy.append(cost[i])
+                                cost_copy.append(cost[j-1])
+                                minn = min(cost_copy)
+                                cost_copy.remove(minn)
+                                res += minn
+                        else:
+                            if s[i] == s[j]:
+                                if cost[i] not in cost_copy and cost[i] != minn:    cost_copy.append(cost[i])
+                                if cost[j] not in cost_copy or cost[j] != minn:     cost_copy.append(cost[j])
+                                minn = min(cost_copy)
+                                cost_copy.remove(minn)
+                                res += minn
+                            else:
+                                minn = 0
+                                cost_copy = []
+            return res
