cb332af6144e54b279478d37776d4db102e0f19c
Atshaya <atshaya1699@gmail.com>
2021-02-02 17:39:13 +0530

    added merge intervals prg in py

---
+++ merge-intervals.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/merge-intervals/
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) < 2:
+            return intervals
+        
+        res = []
+        intervals.sort()
+        i = 0
+        
+        while i < len(intervals) - 1:
+            
+            if intervals[i][1] >= intervals[i+1][0] and intervals[i+1][1] >= intervals[1][0]:
+                max_interval = max(intervals[i][1], intervals[i+1][1])
+                intervals[i+1] = [intervals[i][0], max_interval]
+            else:
+                res.append(intervals[i])
+            if i == len(intervals) - 2:
+                res.append(intervals[i+1])
+
+            i += 1
+            
+        return res
