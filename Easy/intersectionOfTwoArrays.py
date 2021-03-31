18652f9fbabc1f56fd9840d08f25cacee1c59580
Atshaya <atshaya1699@gmail.com>
2021-03-31 13:36:27 +0530

    added intersectionOfTwoArrays

---
+++ Easy/intersectionOfTwoArrays.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        for i in nums1:
+            if i in nums2 and i not in res:
+                res.append(i)
+        return res
