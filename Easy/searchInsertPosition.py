efd98c8919028ce0425ceae7b4b8ec15d8288b69
Atshaya <atshaya1699@gmail.com>
2021-04-03 15:36:04 +0530

    added searchInsertPosition

---
+++ Easy/searchInsertPosition.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)
+        
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] >= target:
+                high = mid
+            else:
+                low = mid + 1
+                
+        return low
