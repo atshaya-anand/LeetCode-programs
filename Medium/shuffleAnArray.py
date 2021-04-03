8e995ebc12b712bd953632925b6b413d9842e659
Atshaya <atshaya1699@gmail.com>
2021-04-03 15:11:14 +0530

    added shuffleAnArray

---
+++ Medium/shuffleAnArray.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/shuffle-an-array/
+
+import random
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.arr = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.arr
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        arrCopy = self.arr[:]
+        random.shuffle(arrCopy)
+        return arrCopy
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
