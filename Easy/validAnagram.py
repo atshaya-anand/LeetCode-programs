812b1c4cc9a7e4795c73eed461cf755423ffc260
Atshaya <atshaya1699@gmail.com>
2021-04-05 13:32:18 +0530

    added validAnagram

---
+++ Easy/validAnagram.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        n1 = len(s)
+        n2 = len(t)
+        if n1 != n2:
+            return False
+        
+        s = sorted(s)
+        t = sorted(t)
+        
+        for i in range(n1):
+            if s[i] != t[i]:
+                return False
+        return True
