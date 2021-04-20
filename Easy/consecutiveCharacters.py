269672a40570ba4b2696f5fe7e80df97bea8ec84
Atshaya <atshaya1699@gmail.com>
2021-04-20 15:56:32 +0530

    added consecutiveCharacters

---
+++ Easy/consecutiveCharacters.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/consecutive-characters/
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        cnt = ans = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                cnt += 1
+                ans = max(cnt, ans)
+            else:
+                cnt = 1
+        return ans
+                
