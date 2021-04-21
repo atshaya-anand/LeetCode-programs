42ea73254940c71a797078042a93a985e2500d56
Atshaya <atshaya1699@gmail.com>
2021-04-21 11:28:02 +0530

    added implementQueueUsingStacks

---
+++ Easy/implementQueueUsingStacks.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/problems/implement-queue-using-stacks/
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        while self.stack1:
+            q = self.stack1.pop()
+            self.stack2.append(q)
+        self.stack1.append(x)
+        while self.stack2:
+            q = self.stack2.pop()
+            self.stack1.append(q)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.stack1.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack1[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if not self.stack1:
+            return True
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
